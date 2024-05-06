6a682f26ce7d7c6cbd7060b0ba1a7541b1551f71
yanupam <yanupam@outlook.com>
2024-05-06 21:25:55 +0530

    added reveison index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // file to add in commit
+/*
+
+go to a new folder you want to make
+type "git init" in the terminal and press enter. This will create a .git file that contains information about your git repository.
+mkdir any changes you want to do
+
+now go to the folder from wher you want to push
+git status
+git add .
+git commit -m "message"
+git remote -v   // checks for remote branches
+
+git remote add origin https://github.com/yanupam/learning02.git to set the remote repository
+
+git push origin master/branch name you want
+
+*/
